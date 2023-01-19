476f88346de2f1a8aad323bab9413c8f5d28dfa3
matheushso <matheushso99@hotmail.com>
2023-01-19 09:15:15 -0300

    create initial page of summary table completed habits

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-import { Habit } from './components/Habit'
+import './styles/global.css'
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <div>
-      <Habit completed={3}/>
-      <Habit completed={10}/>
-      <Habit completed={20}/>
-      <Habit completed={30}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
-
-export default App
